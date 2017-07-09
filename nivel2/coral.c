be598f43fafa5c40ce03dfdd887c6c9174fe7aeb
Igor <igoracm@outlook.com>
2017-07-08 23:47:27 -0300

    retirando o lixo

--- nivel2/coral.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int a, b, c, d;
-
-int main(void) {
-	scanf("%d %d %d %d", &a, &b, &c, &d);
-	char res = ((a == c && b != d) || (b == d && a != c)) ? 'V' : 'F';
-	printf("%c\n", res);
-}
